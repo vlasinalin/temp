3d652ee727e8375d6d4cce8117b7e1b4d72aba98
vlasinalin <vlasin_alin@yahoo.com>
2020-02-02 09:14:40 +0200

    Update bonus.js

--- bonus.js
+++ bonus.js
@@ -1,4 +1,4 @@
-var cursorPayment = db.payment.find({paymentState: "Confirmed", credited: false, paymentDate: {$gt: "2020-01-01"}});
+var cursorPayment = db.payment.find({paymentState: "Confirmed", credited: false, paymentDate: {$gt: new Date("2020-01-01")}});
 while (cursorPayment.hasNext()) {
    printjson(cursorPayment.next());
 }
