10fd70f11ba636177dc6b7c58a66f6ed5ac309fa
vlasinalin <vlasin_alin@yahoo.com>
2020-02-02 09:37:44 +0200

    Update bonus.js

--- bonus.js
+++ bonus.js
@@ -1,4 +1,31 @@
+var userContrib = {};
 var cursorPayment = db.payment.find({paymentState: "Confirmed", credited: false, paymentDate: {$gt: new Date("2020-01-01")}});
 while (cursorPayment.hasNext()) {
-   printjson(cursorPayment.next());
+   var cPayment = cursorPayment.next();
+   var legalEntityId = cPayment.legalEntityId;
+   var track = null;
+   if (cPayment.operation === "MODIFICARE_FIRMA") {
+      track = db.companyModificationTrack.findOne({_id: legalEntityId});
+   }
+   if (cPayment.operation === "SRL-2" || cPayment.operation === "SRL-D-2" || cPayment.operation === "PFA" || cPayment.operation === "II") {
+      track = db.companyCreationTrack.findOne({_id: legalEntityId});
+   }
+   if (track !== null && track.callNotes !== null) {
+      var trackUsers = [];
+      for (var trackIt = 0; trackIt < track.callNotes.length; trackIt++) {
+         var trackUser = track.callNotes[trackIt].user;
+         if (trackUser && !trackUsers.contains(trackUser)) {
+            trackUsers.push(trackUser);
+         }
+      }
+      for (var userIt = 0; userIt < trackUsers.length; userIt++) {
+         var cUser = trackUsers[userIt];
+         if (!userContrib[cUser]) {
+            userContrib[cUser] = 0;
+         } else {
+            userContrib[cUser] = userContrib[cUser] + (1.0 / trackUsers.length);
+         }
+      }
+   }
 }
+printjson(userContrib);
