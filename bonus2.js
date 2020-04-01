b142dded9156201e4dce517715d2d681055d16f2
vlasinalin <vlasin_alin@yahoo.com>
2020-04-01 14:10:27 +0300

    Update bonus2.js

--- bonus2.js
+++ bonus2.js
@@ -1,1 +1,67 @@
-aa
+var userContrib = {};
+var userOperations = {};
+var paymentsValue = 0;
+var paymentsCount = 0;
+var dateFrom = new Date("2020-03-01");
+
+var cursorPayment = db.payment.find({paymentState: "Confirmed", credited: false, paymentDate: { $gt: new Date("2020-03-01"), $lt: new Date("2020-04-01") }});
+
+while (cursorPayment.hasNext()) {
+   var cPayment = cursorPayment.next();
+   var legalEntityId = ObjectId(cPayment.legalEntityId);
+   var track = null;
+   if (cPayment.operation === "MODIFICARE_FIRMA") {
+      paymentsCount++;
+      paymentsValue += cPayment.price;
+      var companyModif = db.companyModification.findOne({_id: legalEntityId})
+      var companyId = ObjectId(companyModif.companyId);
+      track = db.companyModificationTrack.findOne({_id: companyId});
+   }
+   if (cPayment.operation === "SRL-2" || cPayment.operation === "SRL-D-2" || cPayment.operation === "PFA" || cPayment.operation === "II") {
+      paymentsCount++;
+      paymentsValue += cPayment.price;
+      track = db.companyCreationTrack.findOne({_id: legalEntityId});
+   }
+   if (track === null || track.callNotes === null || track.callNotes.length < 1) {
+      print("No track notes:");
+      printjson(cPayment);
+   } else {    
+      var resolutions = {};
+      for (var trackIt = 0; trackIt < track.callNotes.length; trackIt++) {
+         var callNote = track.callNotes[trackIt];
+         if (callNote.callDate > dateFrom && callNote.user && callNote.note) {
+            if (callNote.note === "DOSAR GHISEU VERIFICAT") {
+               resolutions[callNote.note] = callNote.user;
+            }
+            if (callNote.note === "DOSAR DEPUS ONLINE") {
+               resolutions[callNote.note] = callNote.user;
+            }
+            if (callNote.note === "SINCRONIZARE ONRC") {
+               resolutions[callNote.note] = callNote.user;
+            }
+         }
+      }
+      var user;
+      user = resolutions["DOSAR GHISEU VERIFICAT"];
+      if (user) {
+         userContrib[user] = (userContrib[user] || 0) + 10;
+         userOperations["DGV-" + user] =  (userOperations["DGV-" + user] || 0) + 1;
+      }
+      user = resolutions["DOSAR DEPUS ONLINE"];
+      if (user) {
+         userContrib[user] = (userContrib[user] || 0) + 20;
+         userOperations["DDO-" + user] =  (userOperations["DDO-" + user] || 0) + 1;
+      }
+      user = resolutions["SINCRONIZARE ONRC"];
+      if (user) {
+         userContrib[user] = (userContrib[user] || 0) + 5;
+         userOperations["SO-" + user] =  (userOperations["SO-" + user] || 0) + 1;
+      }
+   }
+}
+print("Payments value: " + paymentsValue);
+print("Payments count: " + paymentsCount);
+print("User contrib:");
+printjson(userContrib);
+print("User operations:");
+printjson(userOperations);
